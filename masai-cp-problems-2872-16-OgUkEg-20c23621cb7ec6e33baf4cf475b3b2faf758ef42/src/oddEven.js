9338ad19157920fe3dca839fc566a2bcc25c5043
siyaraam <siyaraam80@gmail.com>
2024-02-26 14:16:18 +0530

    my code

---
+++ masai-cp-problems-2872-16-OgUkEg-20c23621cb7ec6e33baf4cf475b3b2faf758ef42/src/oddEven.js
@@ -0,0 +1,23 @@
+function getData(data) {
+     
+  return new Promise((resolve, reject) => {
+    if (typeof data !== 'number') {
+      reject('error');
+    } else {
+      setTimeout(() => {
+        if (data % 2 === 1) {
+          resolve('odd');
+        } else {
+          resolve('even');
+        }
+      }, data % 2 === 1 ? 2000 : 4000);
+    }
+  });
+}
+
+
+
+    
+
+
+export default getData
