42f2b0a50665822170114d8547db4d50f1d83a75
siyaraam <siyaraam80@gmail.com>
2024-02-26 14:36:43 +0530

    my code

--- masai-cp-problems-2872-16-OgUkEg-20c23621cb7ec6e33baf4cf475b3b2faf758ef42/src/oddEven.js
+++ masai-cp-problems-2872-16-OgUkEg-20c23621cb7ec6e33baf4cf475b3b2faf758ef42/src/oddEven.js
@@ -1,21 +1,22 @@
 function getData(data) {
-     
-  return new Promise((resolve, reject) => {
-    if (typeof data !== 'number') {
-      reject('error');
-    } else {
-      setTimeout(() => {
-        if (data % 2 === 1) {
-          resolve('odd');
-        } else {
-          resolve('even');
-        }
-      }, data % 2 === 1 ? 2000 : 4000);
+    
+
+        return new Promise((resolve, reject) => {
+            if (typeof data !== 'number') {
+                reject('error');
+            } else {
+                setTimeout(() => {
+                    if (data % 2 === 1) {
+                        resolve('odd');
+                    } else {
+                        resolve('even');
+                    }
+                }, data % 2 === 1 ? 2000 : 4000);
+            }
+        });
     }
-  });
-}
 
-
+    module.exports = getData;
 
     
 
